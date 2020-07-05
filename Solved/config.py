bfd127cdd2e9c4b503d2c4481252641bba8496de
Patrick Huynh <p.h.@Patricks-iMac.fios-router.home>
2020-07-05 01:30:26 -0400

    ETL

---
+++ Solved/config.py
@@ -0,0 +1,20 @@
+
+sf_username = 'nhuynh22@gmu.edu'
+sf_password = '2013Love'
+sf_security_token = '4PDgBMb0gYjHRnKEZGUPwzL42'
+
+
+
+# Local Postgres DB Info
+local_db_user = 'postgres'
+local_db_pwd = 'postgres'
+local_db_endpoint = 'localhost'
+local_db_port = '5432'
+local_db_name = 'gwsis'
+
+# Cloud/AWS MySQL DB Info
+remote_db_endpoint = 'database-1.cydllx8fkwb2.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = '2013Love'
+remote_db_name = 'gwsis'
